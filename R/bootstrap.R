1e65f0cb4afd85590fd63d8521a24c0b8b3c79f3
Joel Findlay <63056891+joel23978@users.noreply.github.com>
2026-03-18 01:51:16 +1100

    Update chart font recommendations

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -20,15 +20,16 @@ MAX_SERIES <- 4L
 APP_DATE_FORMATS <- c("%d-%b", "%b-%y", "%b-%Y", "%m-%Y", "%d-%m-%Y", "%Y")
 APP_PALETTES <- c("Okabe-Ito", "Tableau 10", "Set 2", "Dark 3", "R4")
 APP_CHART_FONTS <- c(
-  "Avenir Next",
-  "Helvetica Neue",
-  "Segoe UI",
+  "Plus Jakarta Sans",
   "Inter",
+  "DM Sans",
   "Source Sans 3",
   "IBM Plex Sans",
+  "Roboto",
+  "Libre Franklin",
   "Public Sans",
-  "Work Sans",
-  "Nunito Sans",
-  "Verdana"
+  "Barlow",
+  "Atkinson Hyperlegible",
+  "Noto Sans"
 )
 APP_LEGEND_POSITIONS <- c("bottom", "right", "none")
