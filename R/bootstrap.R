14983423e2d89f263447e4b2854406be9e9a8cd7
Joel Findlay <63056891+joel23978@users.noreply.github.com>
2026-03-18 01:36:06 +1100

    Refresh chart font options

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -20,15 +20,15 @@ MAX_SERIES <- 4L
 APP_DATE_FORMATS <- c("%d-%b", "%b-%y", "%b-%Y", "%m-%Y", "%d-%m-%Y", "%Y")
 APP_PALETTES <- c("Okabe-Ito", "Tableau 10", "Set 2", "Dark 3", "R4")
 APP_CHART_FONTS <- c(
-  "Arial",
-  "Helvetica",
-  "Verdana",
-  "Tahoma",
-  "Trebuchet MS",
-  "Georgia",
-  "Times New Roman",
-  "Garamond",
-  "Palatino",
-  "Courier New"
+  "Avenir Next",
+  "Helvetica Neue",
+  "Segoe UI",
+  "Inter",
+  "Source Sans 3",
+  "IBM Plex Sans",
+  "Public Sans",
+  "Work Sans",
+  "Nunito Sans",
+  "Verdana"
 )
 APP_LEGEND_POSITIONS <- c("bottom", "right", "none")
