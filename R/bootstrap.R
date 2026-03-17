9333ae2ade8255f406338b38f450f86951d4a073
Joel Findlay <63056891+joel23978@users.noreply.github.com>
2026-03-18 02:18:57 +1100

    Add Plus Jakarta Sans chart font

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -20,6 +20,7 @@ MAX_SERIES <- 4L
 APP_DATE_FORMATS <- c("%d-%b", "%b-%y", "%b-%Y", "%m-%Y", "%d-%m-%Y", "%Y")
 APP_PALETTES <- c("Okabe-Ito", "Tableau 10", "Set 2", "Dark 3", "R4")
 APP_CHART_FONTS <- c(
+  "Plus Jakarta Sans",
   "Avenir Next",
   "Avenir",
   "Helvetica Neue",
@@ -28,7 +29,6 @@ APP_CHART_FONTS <- c(
   "Gill Sans",
   "PT Sans",
   "Trebuchet MS",
-  "Arial Narrow",
   "PT Sans Narrow"
 )
 APP_LEGEND_POSITIONS <- c("bottom", "right", "none")
